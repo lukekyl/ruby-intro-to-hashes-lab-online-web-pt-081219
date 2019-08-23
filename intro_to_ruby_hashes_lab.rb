4a48b2bc53d0e2d2c71747e4d71c5f6131640071
Kyle Luke <th.oldschool@gmail.com>
2019-08-23 00:49:11 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -19,9 +19,12 @@ end
 def monopoly_with_second_tier
 	monopoly = {
 	  :railroads => {
-	    :pieces => 4
-	    :names => {}
-	    :rent_in_dollars = {}
+	    :pieces => 4,
+	    :names => {},
+	    :rent_in_dollars => {
+	      one_piece_owned: 25
+	      
+	    }
 	  }
 	}
 
